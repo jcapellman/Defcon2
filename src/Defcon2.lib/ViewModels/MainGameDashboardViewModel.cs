1a6737407c09c5d9849f78922ba03af0c8f6be9a
Jarred Capellman <jcapellman@hotmail.com>
2018-08-05 22:18:25 -0400

    Added: Initial checkin of MainGameDashboardViewModel class

---
+++ src/Defcon2.lib/ViewModels/MainGameDashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defcon2.ViewModels
+{
+    public class MainGameDashboardViewModel : BaseViewModel
+    {
+
+    }
+}
