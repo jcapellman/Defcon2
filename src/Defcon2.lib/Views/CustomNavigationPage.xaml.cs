fb0f831cba9b53f46d59d200800c6e7a6c930cff
Jarred Capellman <jcapellman@hotmail.com>
2018-08-06 22:02:33 -0500

    Added: Initial checkin of the CustomNavigationPage class

---
+++ src/Defcon2.lib/Views/CustomNavigationPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Defcon2.lib.Views
+{
+	public partial class CustomNavigationPage : NavigationPage
+	{
+		public bool IgnoreLayoutChange { get; set; } = false;
+
+		protected override void OnSizeAllocated(double width, double height)
+		{
+		    if (!IgnoreLayoutChange)
+		    {
+		        base.OnSizeAllocated(width, height);
+		    }
+		}
+
+		public CustomNavigationPage() : base()
+		{
+			InitializeComponent();
+		}
+
+		public CustomNavigationPage(Page root) : base(root)
+		{
+			InitializeComponent();
+		}
+	}
+}
