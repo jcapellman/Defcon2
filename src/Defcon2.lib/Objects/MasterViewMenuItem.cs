72767dfd0d6ffa2e9b7b8bbae8a32ff8629e89e6
Jarred Capellman <jcapellman@hotmail.com>
2018-08-06 22:00:35 -0500

    Added: MasterViewMenuItem class

---
+++ src/Defcon2.lib/Objects/MasterViewMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Defcon2.lib.Objects
+{
+    public class MasterViewMenuItem
+    {
+        public MasterViewMenuItem()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
