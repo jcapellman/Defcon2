88207fc51eb6bf97f901e10e2c6914ce051c6564
Jarred Capellman <jcapellman@hotmail.com>
2018-08-06 22:03:47 -0500

    Added: Initial checkin of the MasterView class

---
+++ src/Defcon2.lib/Views/MasterView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Defcon2.lib.Objects;
+
+using Xamarin.Forms;
+
+namespace Defcon2.lib.Views
+{
+	public partial class MasterView : MasterDetailPage
+	{
+		public MasterView()
+		{
+			InitializeComponent();
+
+			Menu.ListView.ItemSelected += ListView_ItemSelected;
+		}
+
+		private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			var item = e.SelectedItem as MasterViewMenuItem;
+
+		    if (item == null)
+		    {
+		        return;
+		    }
+
+		    var page = (Xamarin.Forms.Page)Activator.CreateInstance(item.TargetType);
+
+			Detail = new CustomNavigationPage(page)
+			{
+				BackgroundColor = Color.Transparent,
+				BarBackgroundColor = Color.Transparent
+			};
+
+			IsPresented = false;
+
+			Menu.ListView.SelectedItem = null;
+		}
+	}
+}
