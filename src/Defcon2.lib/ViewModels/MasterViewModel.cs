166fc0c24c8643ca2310d7c3f5c02887cf13b29d
Jarred Capellman <jcapellman@hotmail.com>
2018-08-06 22:01:34 -0500

    Added: Initial checkin of the MasterViewModel class

---
+++ src/Defcon2.lib/ViewModels/MasterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+using Defcon2.lib.Objects;
+using Defcon2.lib.Views;
+
+namespace Defcon2.lib.ViewModels
+{
+    public class MasterViewModel : BaseViewModel
+    {
+        public ObservableCollection<MasterViewMenuItem> MenuItems { get; set; }
+
+		public MasterViewModel()
+        {
+            MenuItems = new ObservableCollection<MasterViewMenuItem>(new[]
+            {
+                new MasterViewMenuItem { Id = 0, Title = "Home", TargetType = typeof(MainGamePage) }
+            });
+        }
+    }
+}
