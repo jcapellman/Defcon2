b7aa5bb74f6b2d14622b605e57d1adc14223b3e0
Jarred Capellman <jcapellman@hotmail.com>
2018-08-06 22:04:11 -0500

    Added: Initial checkin of the MenuView class

---
+++ src/Defcon2.lib/Views/MenuView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Defcon2.lib.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Defcon2.lib.Views
+{
+	public partial class MenuView : ContentPage
+	{
+		public ListView ListView;
+
+		public MenuView()
+		{
+			InitializeComponent();
+
+			BindingContext = new MasterViewModel();
+			ListView = menuItemsListView;
+		}
+	}
+}
